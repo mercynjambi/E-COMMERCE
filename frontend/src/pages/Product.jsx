33ffbe13e6109d7aebc3cfefa31c610d8503f171
mercynjambi <njambim695@gmail.com>
2026-06-22 15:40:30 +0300

    added related products functionality

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -2,6 +2,7 @@ import React, {useEffect, useState, useContext} from 'react'
 import {useParams} from 'react-router-dom'
 import { ShopContext } from '../context/ShopContext';
 import { assets } from '../assets/assets';
+import RelatedProducts from '../components/RelatedProducts'
 
 const Product = () => {
 
@@ -69,9 +70,32 @@ const Product = () => {
              </div>
           </div>
            <button className='bg-black text-white px-8 py-3 text-sm active:bg-gray-700'>ADD TO CART</button>
+          <hr className='mt-8 sm:w-4/5'/>
+          <div className='text-sm text-gray-800 mt-5 flex flex-col gap-1'>
+            <p>100% Original Product.</p>
+            <p>Cash on delivery is available for this product.</p>
+            <p>Easy return and exchange policy within 7 days.</p>
+           </div>
           </div>
-
       </div>
+
+     {/* {Review Section and Description}  */}
+     <div className='mt-20'>
+       <div className='flex'>
+        <b className='border px-5 py-3 text-sm'>Description</b>
+        <p className='border px-5 py-3 text-sm'>Reviews(122)</p>
+        </div>
+        <div className='flex flex-col gap-4 border px-6 py-6 text-sm text-gray-500'>
+          
+          <p>Lorem</p> 
+          <p>Lorem an ecommerce website is a an ecomemerce website</p>         
+        </div>
+     </div>
+
+     {/* {display related products} */}
+     <RelatedProducts category={productData.category}subCategory={productData.subCategory}/>
+     
+
     </div>
   ) : <div className='opacity-0'></div>
 }
