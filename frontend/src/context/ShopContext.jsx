50161514e19b3c968cd062c3b720688e6166fe62
mercynjambi <njambim695@gmail.com>
2026-06-23 22:30:10 +0300

    placing order logic

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import { products } from "../assets/assets";
 import { toast } from "react-toastify";
+import {useNavigate} from "react-router-dom"
 
  export const ShopContext = createContext();
 
@@ -11,6 +12,7 @@ import { toast } from "react-toastify";
     const [search, setSearch] = useState('')
     const [showSearch, setShowSearch] = useState(false)
     const [cartItems, setCartItems] = useState({})
+    const navigate = useNavigate()
 
     const addToCart = async (itemId, size) => {
         if(!size){
@@ -56,13 +58,33 @@ import { toast } from "react-toastify";
     let cartData = structuredClone(cartItems);
     cartData[itemId][size] = quantity;
     setCartItems(cartData);
+  
 
  }
+
+ const getCartAmount =  () =>{
+    let totalAmount = 0;
+    for(const items in cartItems){
+        let itemInfo = products.find((product)=> product._id ===items);
+         for(const item in cartItems[items]){
+            try {
+               if(cartItems[items][item]>0 )  {
+                 totalAmount += itemInfo.price * cartItems[items][item]
+               }
+            }catch(error){
+
+            }            
+         }
+        }
+        return totalAmount;
+
+    }
+ 
 
      const value = {
          products, currency, delivery_fee,
          search,setSearch, showSearch, setShowSearch,
-          cartItems, addToCart, getCartCount, updateQuantity         
+          cartItems, addToCart, getCartCount, updateQuantity, getCartAmount, navigate        
 
      }
 
